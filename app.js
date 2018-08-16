f7bf98751fddb326cb1b869dbde706820bb95594
Sven Anders Robbestad <robbestad@gmail.com>
2018-08-16 15:05:14 +0200

    Intro

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const cards = document.querySelector("#cards");
-fetch("cats.json")
+fetch("https://acandodogapi.azurewebsites.net/dogs.json")
     .then(data => data.json())
     .then(json => {
         json.forEach(entry=>{
             const cardentry = document.createElement("div");
             const img = document.createElement("img");
-            img.src=entry.imageUrl;
+            img.src="https://acandodogapi.azurewebsites.net/"+entry.imageUrl;
             cardentry.append(img);
             cards.append(cardentry);
         })
