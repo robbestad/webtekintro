7118075b8c46f7347379b8f9f56f3844ae488500
Sven Anders Robbestad <robbestad@gmail.com>
2018-08-16 14:58:27 +0200

    Intro

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-var dogs = []
 var cards = document.querySelector("#cards");
 fetch("cats.json")
     .then(data => data.json())
     .then(json => {
         json.forEach(entry=>{
-            console.log(entry.imageUrl)
             let cardentry = document.createElement("div");
             let img = document.createElement("img");
             img.src=entry.imageUrl;
